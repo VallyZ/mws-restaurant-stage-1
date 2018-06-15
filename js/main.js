ff05119062fc91470f02491cdff1377dcc6116a6
Vally <geanny_vally@yahoo.com>
2018-06-15 13:25:35 +0300

    Aria-label for every restaurant on front page

--- js/main.js
+++ js/main.js
@@ -168,6 +168,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', restaurant.name + restaurant.neighborhood + restaurant.address)
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
